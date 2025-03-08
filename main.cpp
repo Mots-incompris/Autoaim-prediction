54803eb724b2ee754e974216e61ef819176a65ca
mots incompris <3670951150@qq.com>
2025-03-08 17:42:56 +0800

    second try

--- main.cpp
+++ main.cpp
@@ -1,6 +1,55 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
 
+//彩色图转灰度二值化图像
+cv::Mat binary(cv::Mat frame)
+{
+    cv::cvtColor(frame,frame, cv::COLOR_BGR2GRAY); 
+
+    int thresholdValue = 128;
+    cv::threshold(frame,frame, thresholdValue, 255, cv::THRESH_BINARY);
+
+    return frame;
+}
+
+// 降噪，腐蚀，膨胀
+cv::Mat dilate(cv::Mat binary)
+{
+    // 高斯模糊，减少噪声
+    cv::GaussianBlur(binary, binary, cv::Size(5, 5), 0);
+
+    // 定义元素结构
+    cv::Mat element = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(9, 9));
+
+    // 腐蚀
+    cv::erode(binary,binary,element);
+
+    // 膨胀
+    cv::dilate(binary,binary,element);
+
+    return binary;
+}
+
+// 查找并绘制轮廓
+cv::Mat contour(cv::Mat dilate,cv::Mat frame)
+{
+    // canny边缘检测
+    cv::Mat edges;
+    cv::Canny(dilate, edges, 100, 200);
+
+    // 查找轮廓
+    std::vector<std::vector<cv::Point>> contours;
+    std::vector<cv::Vec4i> hierarchy;
+    cv::findContours(edges, contours, hierarchy, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+
+    cv::Mat contourImage = cv::Mat::zeros(frame.size(), CV_8UC3);
+
+    // 绘制查找到的轮廓(红框)
+    cv::drawContours(contourImage, contours, -1, cv::Scalar(0, 0, 255), 1);
+
+    return contourImage;
+}
+
 int main()
  {
     // 输入和输出视频文件路径
@@ -9,19 +58,18 @@ int main()
 
     // 打开视频文件
     cv::VideoCapture cap(inputVideoPath);
-    if (!cap.isOpened()) {
+    if (!cap.isOpened())
+    {
         std::cerr << "Error: Could not open video." << std::endl;
         return -1;
     }
-    //原始图像
+    // 原始图像
     cv::Mat frame;
-    // 灰度图像
-    cv::Mat grayImg;
-    //裁剪后的图像
-    cv::Mat croppedImage;
-    //二值化图像
+    // 二值化图像
     cv::Mat binaryImage;
-    //轮廓图像
+    // 腐蚀膨胀图像
+    cv::Mat dilateImage;
+    // 轮廓图像
     cv::Mat contourImage;
 
     // 获取视频的一些基本信息
@@ -36,56 +84,21 @@ int main()
         return -1;
     }
 
-    while (true) {
+    while (true) 
+    {
         // 逐帧读取视频
         cap >> frame; 
-        if (frame.empty()) {
+        if (frame.empty())
             break; // 如果没有更多帧，则退出循环
-        }
-
-        // 裁剪区域（x, y, width, height）
-        int x = 300; // 裁剪区域左上角 x 坐标
-        int y = 400; // 裁剪区域左上角 y 坐标
-        int width = 600; // 裁剪区域的宽度
-        int height = 600; // 裁剪区域的高度
-        cv::Rect roi(x, y, width, height); // 定义感兴趣区域(roi矩形大小的重点)
         
-        // 确保裁剪区域在图像范围内
-        if (x + width <= frame.cols && y + height <= frame.rows) {
-            croppedImage = frame(roi); // 裁剪图像
-        }
-        else{
-            std::cout<< "ROI out of range, skipped." <<std::endl;
-        }
+        // 二值化图像
+        binaryImage = binary(frame);
 
-        //彩色图转灰度图
-        cv::cvtColor(frame, grayImg, cv::COLOR_BGR2GRAY); 
-
-        // 设置二值化阈值，使用灰度图进行二值化操作
-        int thresholdValue = 128;
-        cv::threshold(grayImg, binaryImage, thresholdValue, 255, cv::THRESH_BINARY);
+        // 突出明亮区域
+        dilateImage = dilate(binaryImage);
 
         // 查找轮廓：
-        // 应用高斯模糊，减少噪声
-        cv::GaussianBlur(binaryImage, binaryImage, cv::Size(5, 5), 0);
-
-        // 边缘检测（Canny算法）
-        cv::Mat edges;
-        cv::Canny(binaryImage, edges, 100, 200);
-
-        // 查找轮廓
-        std::vector<std::vector<cv::Point>> contours;
-        std::vector<cv::Vec4i> hierarchy;
-        cv::findContours(edges, contours, hierarchy, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_NONE);
-
-        contourImage = cv::Mat::zeros(frame.size(), CV_8UC3);
-
-        // 绘制查找到的轮廓(红框)
-        cv::drawContours(contourImage, contours, -1, cv::Scalar(0, 0, 255), 1);
-
-        // 在原始图像上绘制ROI矩形
-        cv::rectangle(frame, roi, cv::Scalar(0, 255, 0), 2);
-        cv::putText(frame, "ROI Area", cv::Point(x-10, y-10), cv::FONT_HERSHEY_SIMPLEX, 1, cv::Scalar(255, 0, 0), 2);
+        contourImage = contour(binaryImage,frame);
         
         // 显示结果
         cv::imshow("Contour Image", contourImage);
